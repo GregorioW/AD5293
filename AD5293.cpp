2751c6c2e99c83d48b91108c9fcd5f824f95a50d
Grzegorz Wielgoszewski <Nanometrologia@Wielgoszewski.pl>
2017-12-11 12:04:28 +0100

    Initial commit.
    Signed-off-by: Grzegorz Wielgoszewski <Nanometrologia@Wielgoszewski.pl>

---
+++ AD5293.cpp
@@ -0,0 +1,252 @@
+/*! \file	AD5293.cpp
+ *	\brief	AD5293 class code
+ * 
+ *	\details	Handling of AD5293 digital potentiometer.
+ *
+ *	\pre	SPI is required to operate the AD5293 converter.
+ *	\pre	Therefore, use of <b>SPI</b> library is needed.
+ *
+ *	\date	Created: 30.11.2016 18:43:23
+ *  \author	Grzegorz Wielgoszewski \<Nanometrologia@Wielgoszewski.pl\>
+ */
+
+#include "AD5293.h"
+
+/*!
+ *	\details	Calls the #begin() function for bipolar reference voltage - repeats \a refVoltage as negative and positive value.
+ */
+enum status_code AD5293Class::begin(uint8_t pinCS, float refVoltage) {
+	return this->begin(pinCS, -refVoltage, refVoltage);
+};
+
+/*!
+ *	\details	Assigns Arduino pin for SPI operation of the AD5293 and sets the voltages for potentiometer input (used in output calculations).
+ *				SPI parameters (#m_commSettings) are also set within this function.
+ */
+enum status_code AD5293Class::begin(uint8_t pinCS, float refVoltageBottom, float refVoltageTop) {
+	this->m_bits = 10;
+	
+	this->m_pinCS = pinCS;
+	this->m_bottomRefVoltage = refVoltageBottom;
+	this->m_topRefVoltage = refVoltageTop;
+	this->m_wiper = 0;
+	
+	pinMode(this->m_pinCS, OUTPUT);
+	digitalWrite(this->m_pinCS, HIGH);
+	
+	this->m_commSettings = new SPISettings(500000, MSBFIRST, SPI_MODE1);
+
+	this->placeSDOinHighZ();
+};
+
+/*!
+ *	\details	Assigns Arduino pin for identification of read/write completion.
+ */
+void AD5293Class::setReadyPin(uint8_t pinRDY) {
+	this->m_pinRDY = pinRDY;
+	
+	// By default, the device is ready -> pull up.
+	pinMode(this->m_pinRDY, INPUT_PULLUP);
+}
+
+/*!
+ *	\details	Assigns Arduino pin for SPI operation of the AD5293 and sets the resistances for potentiometer operation (used in value calculations).
+ *				SPI parameters (#m_commSettings) are also set within this function.
+ */
+enum status_code AD5293Class::beginRheo(uint8_t pinCS, uint8_t nomResistance) {
+	this->m_bits = 10;
+
+	this->m_pinCS = pinCS;
+	this->m_bottomResistance = POT_WIPER_RESISTANCE;
+
+	switch (nomResistance) {
+		case 20:
+			this->m_topResistance = 20000.0;
+			break;
+
+		case 50:
+			this->m_topResistance = 50000.0;
+			break;
+
+		case 100:
+			this->m_topResistance = 100000.0;
+			break;
+
+		default:
+			this->m_topResistance = 0.0;
+	}
+	this->m_topResistance += POT_WIPER_RESISTANCE;
+
+	this->m_bottomRefVoltage = 0.0;
+	this->m_topRefVoltage = 0.0;
+
+	this->m_wiper = 0;
+	
+	pinMode(this->m_pinCS, OUTPUT);
+	digitalWrite(this->m_pinCS, HIGH);
+	
+	this->m_commSettings = new SPISettings(500000, MSBFIRST, SPI_MODE1);
+
+	this->placeSDOinHighZ();
+};
+
+/*!
+ *	\details	Assigns Arduino pin for hardware reset.
+ */
+void AD5293Class::setResetPin(uint8_t pinRST) {
+	this->m_pinRST = pinRST;
+	
+	pinMode(this->m_pinRST, OUTPUT);
+	digitalWrite(this->m_pinRST, HIGH);
+}
+
+/*!
+ *	\details	If \c RDY pin is not assigned, \c true is returned after a delay of 10 ms.
+ */
+boolean AD5293Class::isReady() {
+	if (this->m_pinRDY != 255) {
+		return ((digitalRead(m_pinRDY) == 1) ? true : false);
+	} else {
+		delay(10);
+		return true;
+	}
+}
+
+/*!
+ *	\details	After reset, \c SDO pin is switched to high-impedance.
+ */
+void AD5293Class::reset() {
+	if (this->m_pinRST != 255) {
+		digitalWrite(this->m_pinRST, LOW);
+		while (!(digitalRead(this->m_pinRST) == LOW));
+		digitalWrite(this->m_pinRST, HIGH);
+	}
+
+	this->write((uint16_t)1 << (this->m_bits - 1));
+
+	this->placeSDOinHighZ();
+}
+
+/*!
+ *	
+ */
+uint16_t AD5293Class::combine(uint8_t command, uint16_t data) {
+	return (((uint16_t)command << POT_CMD_Pos) | (data & POT_DATA_Msk));
+}
+
+/*!
+ *	\details	See Table 10 in AD5293 datasheet.
+ */
+void AD5293Class::placeSDOinHighZ() {
+	SPI.beginTransaction(*this->m_commSettings);
+	digitalWrite(this->m_pinCS, LOW);
+	SPI.transfer16(POT_CMD_SDO_HIGHZ);
+	digitalWrite(this->m_pinCS, HIGH);
+	while (this->isReady() == false);
+	digitalWrite(this->m_pinCS, LOW);
+	SPI.transfer16(this->combine(POT_CMD_NOP, 0));
+	digitalWrite(this->m_pinCS, HIGH);
+	SPI.endTransaction();
+}
+
+/*!
+ *	\details	See Table 9 in AD5293 datasheet.
+ */
+enum status_code AD5293Class::write(uint16_t wiperSetting) {
+
+	uint16_t receivedData = 0;
+
+	this->placeSDOinHighZ();
+
+	SPI.beginTransaction(*this->m_commSettings);
+
+	digitalWrite(this->m_pinCS, LOW);
+	
+	receivedData = SPI.transfer16(this->combine(POT_CMD_WR_CTRL, POT_CTRL_WIPER_ALLOW_WRITE));
+	digitalWrite(this->m_pinCS, HIGH);
+
+	while (this->isReady() == false);
+	
+	digitalWrite(this->m_pinCS, LOW);
+	receivedData = SPI.transfer16(this->combine(POT_CMD_WRITE, wiperSetting));
+
+	digitalWrite(this->m_pinCS, HIGH);
+	SPI.endTransaction();
+
+	while (this->isReady() == false);
+
+	this->m_wiper = this->getWiper() & POT_DATA_Msk;
+	
+	return STATUS_OK;
+};
+
+/*
+ *	\details	The \a outResistance is trimmed if out of reference range <tt>[#m_bottomResistance, #m_topResistance]</tt>.
+ */
+enum status_code AD5293Class::writeOhms(float outResistance) {
+	float outVal = constrain(outResistance, this->m_bottomResistance, this->m_topResistance);
+	uint16_t outWiper;
+	
+	outWiper = (uint16_t)floorf(((1 << this->m_bits)*(outResistance - this->m_bottomResistance))/(this->m_topResistance - this->m_bottomResistance));
+	outWiper = constrain(outWiper, 0, (1 << this->m_bits) - 1);
+	
+	return this->write(outWiper);
+};
+
+/*
+ *	\details	The \a outVoltage is trimmed if out of reference range <tt>[#m_bottomRefVoltage, #m_topRefVoltage]</tt>.
+ */
+enum status_code AD5293Class::writeVolts(float outVoltage) {
+	float outVal = constrain(outVoltage, this->m_bottomRefVoltage, this->m_topRefVoltage);
+	uint16_t outWiper;
+	
+	outWiper = (uint16_t)floorf(((1 << this->m_bits)*(outVoltage - this->m_bottomRefVoltage))/(this->m_topRefVoltage - this->m_bottomRefVoltage));
+	outWiper = constrain(outWiper, 0, (1 << this->m_bits) - 1);
+	
+	return this->write(outWiper);
+};
+
+/*!
+ *	\details	Within the function, #m_wiper is updated based on the actual read-out from the device. 
+ */
+uint16_t AD5293Class::getWiper() {
+	uint16_t receivedData = 0;
+	
+	SPI.beginTransaction(*this->m_commSettings);
+	
+	digitalWrite(this->m_pinCS, LOW);
+	receivedData = SPI.transfer16(this->combine(POT_CMD_READ, 0x155));
+	digitalWrite(this->m_pinCS, HIGH);
+
+	//  SPI.endTransaction();
+	while (this->isReady() == false);
+	// SPI.beginTransaction(*this->m_commSettings);
+	digitalWrite(this->m_pinCS, LOW);
+	receivedData = SPI.transfer16(this->combine(POT_CMD_NOP, 0x155));
+	digitalWrite(this->m_pinCS, HIGH);
+
+	SPI.endTransaction();
+
+	this->m_wiper = receivedData & POT_DATA_Msk;
+
+	this->placeSDOinHighZ();
+	
+	return receivedData;
+};
+
+/*!
+ *	\details	Read-out from #getWiper() function is recalculated according to #m_topResistance and #m_bottomResistance values.
+ */
+float AD5293Class::getCurrentResistance() {
+	float wiper = float(this->getWiper());
+	return (this->m_bottomResistance + (wiper * (this->m_topResistance- this->m_bottomResistance))/(1 << this->m_bits));
+};
+
+/*!
+ *	\details	Read-out from #getWiper() function is recalculated according to #m_topRefVoltage and #m_bottomRefVoltage values.
+ */
+float AD5293Class::getCurrentVoltage() {
+	float wiper = float(this->getWiper());
+	return (this->m_bottomRefVoltage + (wiper * (this->m_topRefVoltage - this->m_bottomRefVoltage))/(1 << this->m_bits));
+};
+
---
+++ AD5293.h
@@ -0,0 +1,220 @@
+/*! \file	AD5293.h
+ *	\brief	AD5293 class header
+ * 
+ *	\details	Handling of AD5293 digital potentiometer.
+ *
+ *	\pre	SPI is required to operate the AD5293 converter.
+ *	\pre	Therefore, use of <b>SPI</b> library is needed.
+ *
+ *	\date	Created: 30.11.2016 18:43:23
+ *  \author	Grzegorz Wielgoszewski \<Nanometrologia@Wielgoszewski.pl\>
+ */
+
+#ifndef _AD5293_h
+#define _AD5293_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include <SPI.h>
+#if defined(ARDUINO_ARCH_AVR)
+  #include <AVR_status_codes.h>
+#else defined(ARDUINO_ARCH_SAM)
+  #include <status_codes.h>
+#endif
+
+
+/*!
+ *	\brief	Class for the AD5293 digital potentiometer.
+ */
+class AD5293Class
+{
+ private:
+	SPISettings* m_commSettings;	//!< SPI communication settings.
+ 
+	uint8_t m_pinCS;			//!< !SYNC (!CS) pin of the device (Arduino pin).
+	uint8_t m_pinRDY = 255;		//!< RDY pin of the device (Arduino pin).
+	uint8_t m_pinRST = 255;		//!< !RESET pin of the device (Arduino pin).
+	
+	uint16_t m_wiper;			//!< Current wiper position.
+	float m_topRefVoltage;		//!< Top reference voltage value (V).
+	float m_bottomRefVoltage;	//!< Bottom reference voltage value (V).
+
+	float m_topResistance;		//!< Largest resistance (ohms).
+	float m_bottomResistance;	//!< Wiper resistance (ohms).
+	
+	uint8_t m_bits = 10;		//!< Number of wiper setting bits.
+	
+ protected:
+	
+
+ public:
+  
+/*!
+ *	\brief	Configures the potentiometer (symmetric bipolar).
+ *
+ *	\param	pinCS		\c !SYNC pin number (Arduino pin).
+ *	\param	refVoltage	Reference voltage (V).
+ *
+ *	\note	Use this function if the reference voltage is symmetric and bipolar, e.g. -1.024 V to +1.024 V.
+ *
+ *	\returns	\see_status_code
+ */ 
+enum status_code begin(uint8_t pinCS, float refVoltage);
+
+/*!
+ *	\brief	Configures the potentiometer (two separate voltages).
+ *
+ *	\param	pinCS				!SYNC pin number (Arduino pin).
+ *	\param	refVoltageBottom	Lower reference voltage (V).
+ *	\param	refVoltageTop		Upper reference voltage (V).
+ *
+ *	\note	Use this function if the top and bottom reference voltage is unrelated, e.g. 0 to 4.096 V.
+ *
+ *	\returns	\see_status_code
+ */ 
+enum status_code begin(uint8_t pinCS, float refVoltageBottom, float refVoltageTop);
+
+/*!
+ *	\brief	Configures the potentiometer (rheostat).
+ *
+ *	\param	pinCS			\c !SYNC pin number (Arduino pin).
+ *	\param	nomResistance	Nominal resistance (kiloohms).
+ *
+ *	\note	Use this function if the potentiometer acts as a variable resistor in series.
+ *
+ *	\returns	\see_status_code
+ */ 
+enum status_code beginRheo(uint8_t pinCS, uint8_t nomResistance);
+
+/*!
+ *	\brief	Assigns Arduino pin to \c RDY function of AD5293.
+ *
+ *	\param	pinRDY	\c RDY pin number (Arduino pin).
+ *
+ *	\note	\c RDY pin is an optional functionality of AD5293 and requires a suitable board - one of Arduino digital pins needs to be connected to the \c RDY pin.
+ */
+void setReadyPin(uint8_t pinRDY);
+
+/*!
+ *	\brief	Assigns Arduino pin to <tt>!RESET</tt> function of AD5293.
+ *
+ *	\param	pinRST	<tt>!RESET</tt> pin number (Arduino pin).
+ *
+ *	\note	Software <tt>!RESET</tt> may be considered optional in many designs, therefore this functionality requires a suitable board - one of Arduino digital pins needs to be connected to the <tt>!RESET</tt> pin of AD5293.
+ *	\note	In DD00511A, <tt>!RESET</tt> pins of both potentiometers and the D/A converter are connected and driven from the same Arduino pin.
+ */
+void setResetPin(uint8_t pinRST);
+
+/*!
+ *	\brief	Reads the state of \c RDY pin.
+ *	
+ *	\note		\c RDY pin is an optional functionality of AD5293. Please consider proper handling of boards with no connection to \c RDY pin. 
+ *
+ *	\returns	\c RDY pin state if present, \c true otherwise.
+ */
+boolean isReady();
+
+/*!
+ *	\brief	Performs hardware reset.
+ *
+ *	\note	In DD00511A, reset pins of both potentiometers and the D/A converter are connected and driven from the same Arduino pin.
+ *	\note	In DD00521A, reset pins of all potentiometers are driven via logic inverters from a single Arduino/Genuino Micro pin.
+ */
+void reset();
+
+/*! 
+ *	\brief	Combines command code and data.
+ *
+ *	\param	commandCode	Command to be executed.
+ *	\param	data		Data for the command.
+ *
+ *	\returns	Ready-to-send half word.	
+ */
+uint16_t combine(uint8_t commandCode, uint16_t data);
+
+/*!
+ *	\brief	Send command to switch \c SDO pin to high impedance.
+ */
+void placeSDOinHighZ();
+
+/*!
+ *	\brief	Sends the wiper setting as raw value.
+ *
+ *	\param	wiperSetting	Integer value from 0 to 1023 (10-bit potentiometer).
+ *
+ *	\returns	\see_status_code
+ */
+enum status_code write(uint16_t wiperSetting);
+
+/*!
+ *	\brief	Sends the wiper setting as goal value.
+ *
+ *	\param	outResistance	Floating-point number representing resistance to be set (ohms).
+ *
+ *	\returns	\see_status_code
+ */
+enum status_code writeOhms(float outResistance);
+
+/*!
+ *	\brief	Sends the wiper setting as goal value.
+ *
+ *	\param	outVoltage	Floating-point number representing voltage to be set (V).
+ *
+ *	\returns	\see_status_code
+ */
+enum status_code writeVolts(float outVoltage);
+
+/*!
+ *	\brief	Returns current wiper position.
+ *
+ *	\returns	Current wiper position (raw).
+ */
+uint16_t getWiper();
+
+/*!
+ *	\brief	Returns current resistance value.
+ *
+ *	\returns	Current resistance value (ohms).
+ */
+float getCurrentResistance();
+
+/*!
+ *	\brief	Returns current voltage value.
+ *
+ *	\returns	Current voltage value (V).
+ */
+float getCurrentVoltage();
+
+};
+
+// Registers definitions.
+#define POT_CTRL_C2		(0x2u)									//!< Calibration enable.
+#define		POT_CTRL_CAL_RESPERF		(0x0u << POT_CTRL_C2)	//!< Resistor performance mode (default).
+#define		POT_CTRL_CAL_NORMAL			(0x1u << POT_CTRL_C2)	//!< Normal mode.
+#define POT_CTRL_C1		(0x1u)									//!< \c RDAC register write protect.
+#define		POT_CTRL_WIPER_LOCK_ACTIVE	(0x0 << POT_CTRL_C1)	//!< Wiper position locked digitally.
+#define		POT_CTRL_WIPER_ALLOW_WRITE	(0x1 << POT_CTRL_C1)	//!< Allows update of wiper position.
+
+// Command definitions.
+#define	POT_CMD_NOP				(0x00u)		//!< No operation.
+#define POT_CMD_WRITE			(0x01u)		//!< Write to \c RDAC.
+#define POT_CMD_READ			(0x02u)		//!< Read from \c RDAC.
+#define POT_CMD_RESET			(0x04u)		//!< Software reset; refresh with midscale code.
+#define	POT_CMD_WR_CTRL			(0x06u)		//!< Write to control register.
+#define POT_CMD_RD_CTRL			(0x07u)		//!< Read control register.
+#define POT_CMD_SW_POWER_DOWN	(0x08u)		//!< Software power-down.
+#define POT_CMD_SDO_HIGHZ		(0x8001u)	//!< \c SDO into high-Z state.
+
+// Other definitions
+#define POT_CMD_Pos				(0xAu)			//!< Command position in the serial data.
+#define POT_CMD_Msk				(0x3C00u)		//!< Command mask.
+#define POT_DATA_Msk			(0x03FFu)		//!< Wiper setting mask.
+
+#define POT_WIPER_RESISTANCE	(60.0)		//!< Wiper resistance (ohms).
+
+#endif
+
